8afc5acf041efa0b7cef33b3149efbb85c1d95d9
OverDoxeE <158620020+OverDoxeE@users.noreply.github.com>
2024-06-16 20:32:04 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,89 +1,35 @@
-function baza {
-    baza_danych() {
-        read -p "podaj liczbe kolumn: " kolumny
-        read -p "podaj liczbe wierszy (bez naglowka): " wiersze
+echo "Czy chcesz uzyc programu (t/n)"
+read z
 
-        headers=()
-        widths=()
+while [ "$z" == "t" ]; do
+    PS3="Wybierz opcje: "
+    options=("1-Utworz uzytkownika zad.1" "2-Administrator kart sieciowych zad2." "3-Przenies pliki zad3." "4-Utworz baze danych zad.4" "5-Wyjdz")
 
-        echo "Tworzenie naglowka bazy danych"
-        for ((j=0; j<$kolumny; j++)); do
-            read -p "Podaj nazwe dla kolumny $((j+1)): " header
-            headers+=("$header")
-            widths+=(${#header})
-        done
-
-        echo "${headers[*]}" | tr ' ' '|' > baza_danych.txt
-
-        data=()
-
-        echo "Tworzenie bazy danych"
-        for ((i=0; i<$wiersze; i++)); do
-            row=()
-            for ((j=0; j<$kolumny; j++)); do
-                read -p "Podaj ${headers[$j]} dla wiersza $((i+1)): " value
-                row+=("$value")
-                if [ ${#value} -gt ${widths[$j]} ]; then
-                    widths[$j]=${#value}
-                fi
-            done
-            data+=("$(IFS='|'; echo "${row[*]}")")
-            echo "${row[*]}" | tr ' ' '|' >> baza_danych.txt
-        done
-    }
-
-    wyswietl_baze() {
-        wybrane_wiersze=("${!1}")
-        wybrane_kolumny=("${!2}")
-
-        for ((j=0; j<$kolumny; j++)); do
-            if [[ " ${wybrane_kolumny[@]} " =~ " $j " ]]; then
-                printf "%-${widths[$j]}s" "${headers[$j]}"
-                if [ $j -lt $((kolumny-1)) ]; then
-                    printf " | "
-                fi
-            fi
-        done
-        echo
-
-        for row_index in "${wybrane_wiersze[@]}"; do
-            IFS='|' read -r -a values <<< "${data[$((row_index-1))]}"
-            for ((j=0; j<$kolumny; j++)); do
-                if [[ " ${wybrane_kolumny[@]} " =~ " $j " ]]; then
-                    printf "%-${widths[$j]}s" "${values[$j]}"
-                    if [ $j -lt $((kolumny-1)) ]; then
-                        printf " | "
-                    fi
-                fi
-            done
-            echo
-        done
-    }
-
-    baza_danych
-    echo "Baza danych utworzona."
-
-    echo "czy chcesz wyswietlic cala baze danych? (t/n)"
-    read odpRead
-    if [ "$odpRead" == "t" ]; then
-        wybrane_kolumny=($(seq 0 $((kolumny-1))))
-        wybrane_wiersze=($(seq 1 $wiersze))
-        wyswietl_baze wybrane_wiersze[@] wybrane_kolumny[@]
-    fi
-
-    echo "Czy chcesz wyswietlic wybrane wiersze i kolumny? (t/n)"
-    read odpSel
-    if [ "$odpSel" == "t" ]; then
-        echo "Podaj numery wierszy do wyswietlenia (oddzielone spacja): "
-        read -a wybrane_wiersze
-        echo "Podaj numery kolumn do wyswietlenia (oddzielone spacja, zaczynajac od 0): "
-        read -a wybrane_kolumny
-        wyswietl_baze wybrane_wiersze[@] wybrane_kolumny[@]
-    fi
-
-    echo "czy chcesz usunac baze danych? (t/n)"
-    read odpDel
-    if [ "$odpDel" == "t" ]; then
-        rm -f baza_danych.txt
-    fi
-}
+    select opt in "${options[@]}"; do
+        case $opt in
+            "1-Utworz uzytkownika zad.1")
+                grupy
+                break
+                ;;
+            "2-Administrator kart sieciowych zad2.")
+                ipadmin
+                break
+                ;;
+            "3-Przenies pliki zad3.")
+                mover
+                break
+                ;;
+            "4-Utworz baze danych zad.4")
+                baza
+                break
+                ;;
+            "5-Wyjdz")
+                echo "Wyjscie"
+                exit 0
+                ;;
+            *)
+                echo "Podano zla wartosc podaj wartosc z przedzialu 1-5"
+                ;;
+        esac
+    done
+done
